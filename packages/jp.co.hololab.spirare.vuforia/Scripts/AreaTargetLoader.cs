1651bee003d1b49d7f88befbbbbb5ded5174c335
tarukosu <4415085+tarukosu@users.noreply.github.com>
2023-04-17 09:04:54 +0900

    Add jp.co.hololab.spirare.vuforia (#9)

---
+++ packages/jp.co.hololab.spirare.vuforia/Scripts/AreaTargetLoader.cs
@@ -0,0 +1,61 @@
+﻿using HoloLab.PositioningTools.Vuforia;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+#if VUFORIA_PRESENT
+using Vuforia;
+#endif
+
+namespace HoloLab.Spirare.Vuforia
+{
+    public class AreaTargetLoader : MonoBehaviour
+    {
+        private string AreaTargetDataRootPath
+        {
+            get => Path.Combine(Application.persistentDataPath, "AreaTargetData");
+        }
+
+        private void Awake()
+        {
+#if VUFORIA_PRESENT
+            VuforiaApplication.Instance.OnVuforiaStarted += OnVuforiaStarted;
+#endif
+        }
+
+        private void OnVuforiaStarted()
+        {
+#if VUFORIA_PRESENT
+            VuforiaApplication.Instance.OnVuforiaStarted -= OnVuforiaStarted;
+#endif
+            LoadAreaTarget();
+        }
+
+        private void LoadAreaTarget()
+        {
+            var spaceBinderWithVuforiaAreaTarget = GameObject.FindObjectOfType<SpaceBinderWithVuforiaAreaTarget>();
+
+            if (!Directory.Exists(AreaTargetDataRootPath))
+            {
+                Directory.CreateDirectory(AreaTargetDataRootPath);
+            }
+
+            Debug.Log($"Area target data root path: {AreaTargetDataRootPath}");
+
+            var mapFiles = Directory.EnumerateFiles(AreaTargetDataRootPath, "*.xml", SearchOption.AllDirectories);
+
+            foreach (var mapFile in mapFiles)
+            {
+                try
+                {
+                    spaceBinderWithVuforiaAreaTarget.LoadAreaTarget(mapFile);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogWarning(e);
+                }
+            }
+        }
+    }
+}
---
+++ packages/jp.co.hololab.spirare.vuforia/Scripts/VuforiaStatusLog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using HoloLab.PositioningTools.Vuforia;
+#if VUFORIA_PRESENT
+using Vuforia;
+#endif
+
+namespace HoloLab.Spirare.Vuforia
+{
+    public class VuforiaStatusLog : MonoBehaviour
+    {
+        [SerializeField]
+        private SpaceBinderWithVuforiaAreaTarget spaceBinderWithVuforiaAreaTarget;
+
+        [SerializeField]
+        private Text logText;
+
+        private void Awake()
+        {
+            logText.text = null;
+#if VUFORIA_PRESENT
+            spaceBinderWithVuforiaAreaTarget.OnTrackingStatusChanged += OnTrackingStatusChanged;
+#endif
+        }
+
+#if VUFORIA_PRESENT
+        private void OnTrackingStatusChanged(ObserverBehaviour observerBehaviour, TargetStatus targetStatus)
+        {
+            var targetName = observerBehaviour.TargetName;
+            var text = $"{targetName}\n{targetStatus.Status}, {targetStatus.StatusInfo}";
+            logText.text = text;
+        }
+#endif
+    }
+}
