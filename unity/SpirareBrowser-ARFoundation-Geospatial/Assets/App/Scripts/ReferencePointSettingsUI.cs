ecec5d13ea2520aae9357e10c0d342b116827a9f
tarukosu <4415085+tarukosu@users.noreply.github.com>
2023-04-17 12:56:16 +0900

    Add unity project for SpirareBrowser-ARFoundation-Geospatial (#16)

---
+++ unity/SpirareBrowser-ARFoundation-Geospatial/Assets/App/Scripts/ReferencePointSettingsUI.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using HoloLab.PositioningTools.CoordinateSystem;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HoloLab.SpirareBrowser
+{
+    public class ReferencePointSettingsUI : MonoBehaviour
+    {
+        [SerializeField]
+        private WorldCoordinateBinderWithLocationService worldCoordinateBinderWithLocationService = null;
+
+        [SerializeField]
+        private Button bindButton = null;
+
+        [SerializeField]
+        private Button unbindButton = null;
+
+        [SerializeField]
+        private Text bindingInfoText = null;
+
+        [SerializeField]
+        private ReferencePointType referencePointType = ReferencePointType.MainReferencePoint;
+
+        private enum ReferencePointType
+        {
+            MainReferencePoint = 0,
+            SubReferencePoint
+        }
+
+        private void Awake()
+        {
+            bindButton.onClick.AddListener(BindButton_OnClick);
+            unbindButton.onClick.AddListener(UnbindButton_OnClick);
+
+            switch (referencePointType)
+            {
+                case ReferencePointType.MainReferencePoint:
+                    worldCoordinateBinderWithLocationService.OnReferencePointBound += OnBound;
+                    break;
+                case ReferencePointType.SubReferencePoint:
+                    worldCoordinateBinderWithLocationService.OnSubReferencePointBound += OnBound;
+                    break;
+            }
+        }
+
+        private void OnBound(WorldBinding worldBinding)
+        {
+            var geodeticPose = worldBinding.GeodeticPose;
+            var geodeticPosition = geodeticPose.GeodeticPosition;
+
+            var builder = new StringBuilder();
+            builder.AppendLine($"Latitude: {geodeticPosition.Latitude}, Longitude: {geodeticPosition.Longitude}");
+            builder.AppendLine($"EllipsoidalHeight: {geodeticPosition.EllipsoidalHeight}, EnuRotation: {geodeticPose.EnuRotation.eulerAngles}");
+            bindingInfoText.text = builder.ToString();
+        }
+
+        private void OnDestroy()
+        {
+            bindButton.onClick.RemoveListener(BindButton_OnClick);
+            unbindButton.onClick.RemoveListener(UnbindButton_OnClick);
+        }
+
+        private void BindButton_OnClick()
+        {
+            switch (referencePointType)
+            {
+                case ReferencePointType.MainReferencePoint:
+                    worldCoordinateBinderWithLocationService.BindReferencePoint();
+                    return;
+                case ReferencePointType.SubReferencePoint:
+                    worldCoordinateBinderWithLocationService.BindSubReferencePoint();
+                    return;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void UnbindButton_OnClick()
+        {
+            bindingInfoText.text = "";
+            switch (referencePointType)
+            {
+                case ReferencePointType.MainReferencePoint:
+                    worldCoordinateBinderWithLocationService.UnbindReferencePoint();
+                    return;
+                case ReferencePointType.SubReferencePoint:
+                    worldCoordinateBinderWithLocationService.UnbindSubReferencePoint();
+                    return;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ unity/SpirareBrowser-ARFoundation-Geospatial/README.md
@@ -0,0 +1,19 @@
+# SpirareBrowser-ARFoundation-Geospatial
+
+This project is a browser application for Spirare.
+
+## Target Devices
+ARFoundation supported devices
+
+## Position Alignment Method
+ARCore Geospatial API
+
+## Project Setup
+
+Create an ARCore API key.  
+Please refer to [the documentation](https://developers.google.com/ar/develop/unity-arf/geospatial/enable-android#api_key_authorization) for more information.
+
+Open the unity project.  
+Select **Edit** > **ProjectSettings** > **XR Plug-in Management** > **ARCore Extensions** and paste the API key in the API Key field.
+
+<img width="480" alt="ARCore Extensions API key settings" src="https://user-images.githubusercontent.com/4415085/223650258-8157f411-1624-459c-976b-58edb4504569.png">
