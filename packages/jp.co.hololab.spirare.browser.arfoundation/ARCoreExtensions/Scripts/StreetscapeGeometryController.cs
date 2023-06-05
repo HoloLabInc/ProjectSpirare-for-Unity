15d46cea4b00254e99e76a06a9d7bd8aa8a3b6a7
tarukosu <4415085+tarukosu@users.noreply.github.com>
2023-06-05 13:44:11 +0900

    Add streetscape occlusion feature for SpirareBrowser-ARFoundation-Geospatial (#45)

---
+++ packages/jp.co.hololab.spirare.browser.arfoundation/ARCoreExtensions/Scripts/StreetscapeGeometryController.cs
@@ -0,0 +1,124 @@
+#if ARCOREEXTENSIONS_1_37_0_OR_NEWER
+using Google.XR.ARCoreExtensions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR.ARSubsystems;
+#endif
+using UnityEngine;
+
+namespace HoloLab.Spirare.Browser.ARFoundation.ARCoreExtensions
+{
+    public class StreetscapeGeometryController : MonoBehaviour
+    {
+#if ARCOREEXTENSIONS_1_37_0_OR_NEWER
+        [SerializeField]
+        private ARStreetscapeGeometryManager arStreetscapeGeometryManager;
+
+        [SerializeField]
+        private Material streetscapeGeometryMaterial;
+
+        private readonly Dictionary<TrackableId, GameObject> streetscapeGeometryGameObjects =
+            new Dictionary<TrackableId, GameObject>();
+
+        private void OnEnable()
+        {
+            arStreetscapeGeometryManager.StreetscapeGeometriesChanged += StreetscapeGeometriesChanged;
+        }
+
+        private void OnDisable()
+        {
+            arStreetscapeGeometryManager.StreetscapeGeometriesChanged -= StreetscapeGeometriesChanged;
+
+            DestroyAllGeometryObjects();
+        }
+
+        private void StreetscapeGeometriesChanged(ARStreetscapeGeometriesChangedEventArgs eventArgs)
+        {
+            AddGeometries(eventArgs.Added);
+            UpdateGeometries(eventArgs.Updated);
+            RemoveGeometries(eventArgs.Removed);
+        }
+
+        private void AddGeometries(List<ARStreetscapeGeometry> added)
+        {
+            foreach (var streetscapeGeometry in added)
+            {
+                CreateGeometryObject(streetscapeGeometry);
+            }
+        }
+
+        private void UpdateGeometries(List<ARStreetscapeGeometry> updated)
+        {
+            foreach (var streetscapeGeometry in updated)
+            {
+                CreateGeometryObject(streetscapeGeometry);
+                UpdateGeometryObject(streetscapeGeometry);
+            }
+        }
+
+        private void RemoveGeometries(List<ARStreetscapeGeometry> removed)
+        {
+            foreach (var streetscapeGeometry in removed)
+            {
+                DestroyGeometryObject(streetscapeGeometry);
+            }
+        }
+
+        private void CreateGeometryObject(ARStreetscapeGeometry streetscapeGeometry)
+        {
+            if (streetscapeGeometry.mesh == null)
+            {
+                return;
+            }
+
+            var trackableId = streetscapeGeometry.trackableId;
+
+            if (streetscapeGeometryGameObjects.ContainsKey(trackableId))
+            {
+                return;
+            }
+
+            GameObject renderObject = new GameObject(
+                           "StreetscapeGeometryMesh", typeof(MeshFilter), typeof(MeshRenderer));
+
+            if (renderObject)
+            {
+                renderObject.transform.parent = transform;
+                renderObject.transform.SetPositionAndRotation(streetscapeGeometry.pose.position, streetscapeGeometry.pose.rotation);
+                renderObject.GetComponent<MeshFilter>().mesh = streetscapeGeometry.mesh;
+                renderObject.GetComponent<MeshRenderer>().material = streetscapeGeometryMaterial;
+
+                streetscapeGeometryGameObjects[trackableId] = renderObject;
+            }
+        }
+
+        private void UpdateGeometryObject(ARStreetscapeGeometry streetscapeGeometry)
+        {
+            if (streetscapeGeometryGameObjects.TryGetValue(streetscapeGeometry.trackableId, out var renderObject))
+            {
+                renderObject.transform.SetPositionAndRotation(streetscapeGeometry.pose.position, streetscapeGeometry.pose.rotation);
+            }
+        }
+
+        private void DestroyGeometryObject(ARStreetscapeGeometry streetscapeGeometry)
+        {
+            if (streetscapeGeometryGameObjects.TryGetValue(streetscapeGeometry.trackableId, out var renderObject))
+            {
+                streetscapeGeometryGameObjects.Remove(streetscapeGeometry.trackableId);
+                Destroy(renderObject);
+            }
+        }
+
+        private void DestroyAllGeometryObjects()
+        {
+            foreach (var pair in streetscapeGeometryGameObjects)
+            {
+                Destroy(pair.Value);
+            }
+
+            streetscapeGeometryGameObjects.Clear();
+        }
+#endif
+    }
+}
