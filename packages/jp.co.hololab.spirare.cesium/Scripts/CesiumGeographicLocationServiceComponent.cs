6a4c1583d26413b7ca2c4826a5c3d22c58aa3f42
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2023-06-23 14:38:55 +0900

    update

--- packages/jp.co.hololab.spirare.cesium/Scripts/CesiumGeographicLocationServiceComponent.cs
+++ packages/jp.co.hololab.spirare.cesium/Scripts/CesiumGeographicLocationServiceComponent.cs
@@ -1,44 +1,115 @@
+using CesiumForUnity;
 using HoloLab.PositioningTools;
+using HoloLab.PositioningTools.GeographicCoordinate;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.Mathematics;
 using UnityEngine;
 
-public class CesiumGeographicLocationServiceComponent : MonoBehaviour, IGeographicLocationService, ICardinalDirectionService
+namespace HoloLab.Spirare.Cesium
 {
-    private bool serviceEnabled;
-
-    public event Action<GeographicLocation> OnLocationUpdated;
-    public event Action<CardinalDirection> OnDirectionUpdated;
-
-    private void Update()
+    public class CesiumGeographicLocationServiceComponent : MonoBehaviour, IGeographicLocationService, ICardinalDirectionService
     {
-        if (serviceEnabled)
+        [SerializeField]
+        private Transform targetTransform = null;
+
+        CesiumGeoreference cesiumGeoreference;
+
+        private bool serviceEnabled;
+
+        public event Action<GeographicLocation> OnLocationUpdated;
+        public event Action<CardinalDirection> OnDirectionUpdated;
+
+        private void Awake()
         {
-            // TODO implement
+            if (targetTransform == null)
+            {
+                targetTransform = Camera.main.transform;
+            }
+
+            cesiumGeoreference = FindFirstObjectByType<CesiumGeoreference>();
         }
-    }
 
-    public void StartService()
-    {
-        _ = StartServiceAsync();
-    }
+        private void Update()
+        {
+            if (serviceEnabled)
+            {
+                NotifyLocation();
+            }
+        }
 
-    public Task<(bool ok, Exception exception)> StartServiceAsync()
-    {
-        serviceEnabled = true;
-        return Task.FromResult<(bool, Exception)>((true, null));
-    }
+        public void StartService()
+        {
+            _ = StartServiceAsync();
+        }
 
-    public void StopService()
-    {
-        _ = StopServiceAsync();
-    }
+        public Task<(bool ok, Exception exception)> StartServiceAsync()
+        {
+            if (cesiumGeoreference == null)
+            {
+                return Task.FromResult<(bool, Exception)>((false, new InvalidOperationException("CesiumGeoreference not found")));
+            }
+            else
+            {
+                serviceEnabled = true;
+                return Task.FromResult<(bool, Exception)>((true, null));
+            }
+        }
 
-    public Task<(bool ok, Exception exception)> StopServiceAsync()
-    {
-        serviceEnabled = false;
-        return Task.FromResult<(bool, Exception)>((true, null));
+        public void StopService()
+        {
+            _ = StopServiceAsync();
+        }
+
+        public Task<(bool ok, Exception exception)> StopServiceAsync()
+        {
+            serviceEnabled = false;
+            return Task.FromResult<(bool, Exception)>((true, null));
+        }
+
+        private void NotifyLocation()
+        {
+            if (cesiumGeoreference == null)
+            {
+                return;
+            }
+
+            var unityPosition = new double3(targetTransform.position);
+            var ecefPosition = cesiumGeoreference.TransformUnityPositionToEarthCenteredEarthFixed(unityPosition);
+            var geodeticPosition = GeographicCoordinateConversion.EcefToGeodetic(ecefPosition.x, ecefPosition.y, ecefPosition.z);
+
+            var now = DateTimeOffset.Now;
+            var geographicLocation = new GeographicLocation(geodeticPosition.Latitude, geodeticPosition.Longitude, geodeticPosition.EllipsoidalHeight, now);
+
+            // Cardinal direction
+            var unityDirection = new double3(targetTransform.forward);
+            var ecefDirection = cesiumGeoreference.TransformUnityDirectionToEarthCenteredEarthFixed(unityDirection);
+            var enuDirection = GeographicCoordinateConversion.EcefToEnu(
+                ecefPosition.x + ecefDirection.x, ecefPosition.y + ecefDirection.y, ecefPosition.z + ecefDirection.z,
+                geodeticPosition.Latitude, geodeticPosition.Longitude, geodeticPosition.EllipsoidalHeight);
+            var headingDegrees = (float)(math.atan2(enuDirection.East, enuDirection.North) * 180.0 / math.PI);
+
+            var cardinalDirection = new CardinalDirection(headingDegrees, now);
+
+            try
+            {
+                OnLocationUpdated?.Invoke(geographicLocation);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+
+            try
+            {
+                OnDirectionUpdated?.Invoke(cardinalDirection);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
     }
 }
