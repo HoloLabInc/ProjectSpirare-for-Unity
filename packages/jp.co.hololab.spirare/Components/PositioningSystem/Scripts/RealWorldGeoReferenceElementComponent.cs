13cb24d180f9cf310d84c2fb2034d7beea057b49
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2023-06-26 09:37:20 +0900

    update

--- packages/jp.co.hololab.spirare/Components/PositioningSystem/Scripts/RealWorldGeoReferenceElementComponent.cs
+++ packages/jp.co.hololab.spirare/Components/PositioningSystem/Scripts/RealWorldGeoReferenceElementComponent.cs
@@ -1,5 +1,6 @@
 using HoloLab.PositioningTools.CoordinateSystem;
 using HoloLab.PositioningTools.GeographicCoordinate;
+using System;
 using UnityEngine;
 
 namespace HoloLab.Spirare
@@ -7,20 +8,16 @@ namespace HoloLab.Spirare
     public sealed class RealWorldGeoReferenceElementComponent : GeoReferenceElementComponent
     {
         private WorldCoordinateOrigin worldCoordinateOrigin;
+        private CoordinateManager coordinateManager;
 
         private void Start()
         {
+            // For update via websocket
             if (TryGetComponent<PomlObjectElementComponent>(out var elementComponent))
             {
                 elementComponent.OnElementUpdated += _ =>
                 {
-                    if (worldCoordinateOrigin != null)
-                    {
-                        UpdateGameObject();
-
-                        var wb = CoordinateManager.Instance.LatestWorldBinding;
-                        worldCoordinateOrigin.BindCoordinates(wb);
-                    }
+                    UpdateGameObject();
                 };
             }
         }
@@ -32,12 +29,38 @@ namespace HoloLab.Spirare
             GeoReferenceElement = geoReferenceElement;
             worldCoordinateOrigin = gameObject.AddComponent<WorldCoordinateOrigin>();
 
+            coordinateManager = CoordinateManager.Instance;
+
             UpdateGameObject();
+            OnElementUpdated += PomlElementComponent_UpdateGameObject;
 
             return this;
         }
 
+        private void PomlElementComponent_UpdateGameObject(PomlElement element)
+        {
+            UpdateGameObject();
+        }
+
         private void UpdateGameObject()
+        {
+            UpdateWorldCoordinateOrigin();
+            BindCoordinates();
+        }
+
+        private void BindCoordinates()
+        {
+            if (coordinateManager != null && worldCoordinateOrigin != null)
+            {
+                var worldBinding = coordinateManager.LatestWorldBinding;
+                if (worldBinding != null)
+                {
+                    worldCoordinateOrigin.BindCoordinates(worldBinding);
+                }
+            }
+        }
+
+        private void UpdateWorldCoordinateOrigin()
         {
             if (worldCoordinateOrigin == null)
             {
