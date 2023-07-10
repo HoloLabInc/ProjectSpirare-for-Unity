66cb1a0ebb9ab3eb95bc791baf7a460f2be90d37
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2023-07-11 08:50:33 +0900

    update

--- packages/jp.co.hololab.spirare/Scripts/Components/ModelElementComponent.cs
+++ packages/jp.co.hololab.spirare/Scripts/Components/ModelElementComponent.cs
@@ -6,12 +6,12 @@ namespace HoloLab.Spirare
     public enum PomlElementLoadingStatus
     {
         NotLoaded,
+        UnknownError,
         DataFetching,
         Loading,
         Loaded,
         DataFetchError,
-        LoadError,
-        Error,
+        LoadError
     }
 
     public abstract class ModelElementComponent : SpecificObjectElementComponentBase<PomlModelElement>, IWithinCamera
