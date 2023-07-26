f67afdd5d4815bbdcc3e6f155e03c7e9e4cbbe84
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2023-07-26 10:48:43 +0900

    update

---
+++ packages/jp.co.hololab.spirare.cesium/Scripts/Cesium3dTilesetPointCloudSettingsApplier.cs
@@ -0,0 +1,55 @@
+using CesiumForUnity;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace HoloLab.Spirare.Cesium
+{
+    [RequireComponent(typeof(Cesium3DTileset))]
+    public class Cesium3dTilesetPointCloudSettingsApplier : MonoBehaviour
+    {
+        [SerializeField]
+        private PointCloudRenderSettings pointCloudRenderSettings;
+
+        private Cesium3DTileset cesium3DTileset;
+        private PropertyInfo pointSizeProperty;
+
+        private void Awake()
+        {
+            cesium3DTileset = GetComponent<Cesium3DTileset>();
+
+            var pointCloudShading = cesium3DTileset.pointCloudShading;
+            var pointCloudShadingType = pointCloudShading.GetType();
+            pointSizeProperty = pointCloudShadingType.GetProperty("pointSize", BindingFlags.Public | BindingFlags.Instance);
+
+            if (pointCloudRenderSettings != null)
+            {
+                pointCloudRenderSettings.OnPointSizeChanged += OnPointSizeChanged;
+            }
+
+            ApplySettings();
+        }
+
+        private void OnPointSizeChanged(float pointSize)
+        {
+            ApplySettings();
+        }
+
+        private void ApplySettings()
+        {
+            var pointCloudShading = cesium3DTileset.pointCloudShading;
+
+            if (pointCloudRenderSettings == null || pointCloudRenderSettings.PointSize == 0)
+            {
+                pointCloudShading.attenuation = false;
+            }
+            else
+            {
+                pointCloudShading.attenuation = true;
+
+                // Use reflection to set pointSize because pointSize proerty exists in the customized version.
+                pointSizeProperty?.SetValue(pointCloudShading, pointCloudRenderSettings.PointSize);
+            }
+        }
+    }
+}
--- packages/jp.co.hololab.spirare/Components/Common/Scripts/PointCloudRenderSettings.cs
+++ packages/jp.co.hololab.spirare/Components/Common/Scripts/PointCloudRenderSettings.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,34 @@ namespace HoloLab.Spirare
 {
     public class PointCloudRenderSettings : ScriptableObject
     {
-        public float pointSize = 0f;
+        [SerializeField]
+        private float pointSize = 0f;
+
+        public float PointSize
+        {
+            get
+            {
+                return pointSize;
+            }
+            set
+            {
+                pointSize = value;
+                InvokeOnPointSizeChanged();
+            }
+        }
+
+        public event Action<float> OnPointSizeChanged;
+
+        private void InvokeOnPointSizeChanged()
+        {
+            try
+            {
+                OnPointSizeChanged?.Invoke(pointSize);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
     }
 }
