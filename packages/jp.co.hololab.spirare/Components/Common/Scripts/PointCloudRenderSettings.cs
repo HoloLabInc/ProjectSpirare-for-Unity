262a3b1fc40a762479a31395ea803e4337c864d3
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2023-07-26 10:16:50 +0900

    update

--- packages/jp.co.hololab.spirare/Components/Common/Scripts/PointCloudRenderSettings.cs
+++ packages/jp.co.hololab.spirare/Components/Common/Scripts/PointCloudRenderSettings.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 namespace HoloLab.Spirare
 {
-    [CreateAssetMenu(fileName = "PointCloudRenderSettings", menuName = "Spirare/PointCloudRenderSettings", order = 1)]
     public class PointCloudRenderSettings : ScriptableObject
     {
         public float pointSize = 0f;
