46c535045afe72bcba1ad54146c170890d3853fe
tarukosu <4415085+tarukosu@users.noreply.github.com>
2023-09-04 13:14:01 +0900

    Move TilesetSourceSetter.cs to jp.co.hololab.spirare.cesium package (#84)

--- packages/jp.co.hololab.spirare.cesium/Scripts/TilesetSourceSettings.cs
+++ packages/jp.co.hololab.spirare.cesium/Scripts/TilesetSourceSettings.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace HoloLab.Spirare.Browser.Desktop.Cesium
+namespace HoloLab.Spirare.Cesium
 {
     public class TilesetSourceSettings : ScriptableObject
     {
