bd6ef981ee306c111587eec22035c17732cf8ecd
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2023-11-24 12:22:23 +0900

    update

--- unity/SpirareBrowser-Quest-Cesium/Assets/App/HttpServer/GeodeticSettingsHttpController.cs
+++ unity/SpirareBrowser-Quest-Cesium/Assets/App/HttpServer/GeodeticSettingsHttpController.cs
@@ -10,50 +10,52 @@ using System.Net;
 using System.Threading.Tasks;
 using UnityEngine;
 
-public class GeodeticSettingsHttpController : MonoBehaviour, IHttpController
+namespace HoloLab.Spirare.Browser.Quest.Cesium
 {
-    [SerializeField]
-    private CesiumGeoreference cesiumGeoreference;
-
-    [SerializeField]
-    private Transform cameraOriginTransform;
-
-    private const string latitudeSaveKey = "GeodeticSettingsHttpController_latitude";
-    private const string longitudeSaveKey = "GeodeticSettingsHttpController_longitude";
-    private const string heightSaveKey = "GeodeticSettingsHttpController_height";
-
-    private void Start()
+    public class GeodeticSettingsHttpController : MonoBehaviour, IHttpController
     {
-        var savedLatitudeString = PlayerPrefs.GetString(latitudeSaveKey);
-        var savedLongitudeString = PlayerPrefs.GetString(longitudeSaveKey);
-        var savedHeight = PlayerPrefs.GetFloat(heightSaveKey);
+        [SerializeField]
+        private CesiumGeoreference cesiumGeoreference;
 
-        var latitude = cesiumGeoreference.latitude;
-        var longitude = cesiumGeoreference.longitude;
-        var height = cesiumGeoreference.height;
+        [SerializeField]
+        private Transform cameraOriginTransform;
 
-        if (double.TryParse(savedLatitudeString, out var savedLatitude))
+        private const string latitudeSaveKey = "GeodeticSettingsHttpController_latitude";
+        private const string longitudeSaveKey = "GeodeticSettingsHttpController_longitude";
+        private const string heightSaveKey = "GeodeticSettingsHttpController_height";
+
+        private void Start()
         {
-            latitude = savedLatitude;
-        }
-        if (double.TryParse(savedLongitudeString, out var savedLongitude))
-        {
-            longitude = savedLongitude;
-        }
-        if (savedHeight != 0)
-        {
-            height = savedHeight;
+            var savedLatitudeString = PlayerPrefs.GetString(latitudeSaveKey);
+            var savedLongitudeString = PlayerPrefs.GetString(longitudeSaveKey);
+            var savedHeight = PlayerPrefs.GetFloat(heightSaveKey);
+
+            var latitude = cesiumGeoreference.latitude;
+            var longitude = cesiumGeoreference.longitude;
+            var height = cesiumGeoreference.height;
+
+            if (double.TryParse(savedLatitudeString, out var savedLatitude))
+            {
+                latitude = savedLatitude;
+            }
+            if (double.TryParse(savedLongitudeString, out var savedLongitude))
+            {
+                longitude = savedLongitude;
+            }
+            if (savedHeight != 0)
+            {
+                height = savedHeight;
+            }
+
+            cesiumGeoreference.SetOriginLongitudeLatitudeHeight(longitude, latitude, height);
         }
 
-        cesiumGeoreference.SetOriginLongitudeLatitudeHeight(longitude, latitude, height);
-    }
-
-    [Route("/geodetic-settings")]
-    public string Index()
-    {
-        var latLon = $"{cesiumGeoreference.latitude},{cesiumGeoreference.longitude}";
-        var height = cesiumGeoreference.height;
-        var html = $@"
+        [Route("/geodetic-settings")]
+        public string Index()
+        {
+            var latLon = $"{cesiumGeoreference.latitude},{cesiumGeoreference.longitude}";
+            var height = cesiumGeoreference.height;
+            var html = $@"
 <html>
   <body>
     <a href=""/"">Back</a>
@@ -80,77 +82,78 @@ public class GeodeticSettingsHttpController : MonoBehaviour, IHttpController
   </body>
 </html>
 ";
-        return html;
-    }
+            return html;
+        }
 
-    [Route("/geodetic-settings/position")]
-    public async Task ChangePosition(HttpListenerRequest request, HttpListenerResponse response)
-    {
-        if (request.HttpMethod == "POST")
+        [Route("/geodetic-settings/position")]
+        public async Task ChangePosition(HttpListenerRequest request, HttpListenerResponse response)
         {
-            var reader = new StreamReader(request.InputStream);
-            var body = await reader.ReadToEndAsync();
-
-            var queries = HttpQueryParser.ParseQueryString(body);
-
-            if (queries.TryGetValue("latlon", out var latLonString))
+            if (request.HttpMethod == "POST")
             {
-                if (TryParseLatitudeLongitude(latLonString, out var latitude, out var longitude))
-                {
-                    var height = cesiumGeoreference.height;
-                    cesiumGeoreference.SetOriginLongitudeLatitudeHeight(longitude, latitude, height);
+                var reader = new StreamReader(request.InputStream);
+                var body = await reader.ReadToEndAsync();
 
-                    PlayerPrefs.SetString(latitudeSaveKey, latitude.ToString());
-                    PlayerPrefs.SetString(longitudeSaveKey, longitude.ToString());
-                    PlayerPrefs.Save();
+                var queries = HttpQueryParser.ParseQueryString(body);
+
+                if (queries.TryGetValue("latlon", out var latLonString))
+                {
+                    if (TryParseLatitudeLongitude(latLonString, out var latitude, out var longitude))
+                    {
+                        var height = cesiumGeoreference.height;
+                        cesiumGeoreference.SetOriginLongitudeLatitudeHeight(longitude, latitude, height);
+
+                        PlayerPrefs.SetString(latitudeSaveKey, latitude.ToString());
+                        PlayerPrefs.SetString(longitudeSaveKey, longitude.ToString());
+                        PlayerPrefs.Save();
+                    }
                 }
+
+                if (queries.TryGetValue("height", out var heightString))
+                {
+                    if (TryParseFloat(heightString, out var height))
+                    {
+                        var longitude = cesiumGeoreference.longitude;
+                        var latitude = cesiumGeoreference.latitude;
+                        cesiumGeoreference.SetOriginLongitudeLatitudeHeight(longitude, latitude, height);
+
+                        PlayerPrefs.SetFloat(heightSaveKey, height);
+                    }
+                }
+
+                cameraOriginTransform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
             }
 
-            if (queries.TryGetValue("height", out var heightString))
-            {
-                if (TryParseFloat(heightString, out var height))
-                {
-                    var longitude = cesiumGeoreference.longitude;
-                    var latitude = cesiumGeoreference.latitude;
-                    cesiumGeoreference.SetOriginLongitudeLatitudeHeight(longitude, latitude, height);
+            response.Redirect("/geodetic-settings");
+        }
 
-                    PlayerPrefs.SetFloat(heightSaveKey, height);
-                }
+        private static bool TryParseFloat(string text, out float value)
+        {
+            try
+            {
+                var dt = new DataTable();
+                value = (float)Convert.ToDouble(dt.Compute(text, ""));
+                return true;
+            }
+            catch (Exception)
+            {
+                value = 0;
+                return false;
+            }
+        }
+
+        private static bool TryParseLatitudeLongitude(string text, out double latitude, out double longitude)
+        {
+            latitude = 0;
+            longitude = 0;
+
+            var separator = new char[] { ',', ' ' };
+            var tokens = text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            if (tokens.Length <= 1)
+            {
+                return false;
             }
 
-            cameraOriginTransform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+            return double.TryParse(tokens[0], out latitude) && double.TryParse(tokens[1], out longitude);
         }
-
-        response.Redirect("/geodetic-settings");
-    }
-
-    private static bool TryParseFloat(string text, out float value)
-    {
-        try
-        {
-            var dt = new DataTable();
-            value = (float)Convert.ToDouble(dt.Compute(text, ""));
-            return true;
-        }
-        catch (Exception)
-        {
-            value = 0;
-            return false;
-        }
-    }
-
-    private static bool TryParseLatitudeLongitude(string text, out double latitude, out double longitude)
-    {
-        latitude = 0;
-        longitude = 0;
-
-        var separator = new char[] { ',', ' ' };
-        var tokens = text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
-        if (tokens.Length <= 1)
-        {
-            return false;
-        }
-
-        return double.TryParse(tokens[0], out latitude) && double.TryParse(tokens[1], out longitude);
     }
 }
