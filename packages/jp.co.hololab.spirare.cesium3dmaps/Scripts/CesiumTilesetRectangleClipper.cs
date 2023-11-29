c4b0e92238a4ac1b58bbbeca966903da604d2b22
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2023-11-29 13:51:52 +0900

    update

--- packages/jp.co.hololab.spirare.cesium3dmaps/Scripts/CesiumTilesetRectangleClipper.cs
+++ packages/jp.co.hololab.spirare.cesium3dmaps/Scripts/CesiumTilesetRectangleClipper.cs
@@ -4,56 +4,59 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-[RequireComponent(typeof(Cesium3DTileset))]
-public class CesiumTilesetRectangleClipper : MonoBehaviour
+namespace HoloLab.Spirare.Cesium3DMaps
 {
-    private Cesium3DTileset tileset;
-
-    private List<(GameObject TileObject, Material[] Materials)> tiles
-        = new List<(GameObject TileObject, Material[] Materials)>();
-
-    public Transform ClippingOriginTransform { get; set; }
-
-    public float ClippingSizeX { get; set; } = 1;
-
-    public float ClippingSizeZ { get; set; } = 1;
-
-    private void Start()
+    [RequireComponent(typeof(Cesium3DTileset))]
+    public class CesiumTilesetRectangleClipper : MonoBehaviour
     {
-        tileset = GetComponent<Cesium3DTileset>();
-        tileset.OnTileGameObjectCreated += Tileset_OnTileGameObjectCreated;
-    }
+        private Cesium3DTileset tileset;
 
-    private void Tileset_OnTileGameObjectCreated(GameObject tileObject)
-    {
-        var meshRenderers = tileObject.GetComponentsInChildren<MeshRenderer>();
-        var materials = meshRenderers.Select(x => x.material).ToArray();
-        tiles.Add((tileObject, materials));
-    }
+        private readonly List<(GameObject TileObject, Material[] Materials)> tiles
+            = new List<(GameObject TileObject, Material[] Materials)>();
 
-    private void LateUpdate()
-    {
-        if (ClippingOriginTransform == null)
+        public Transform ClippingOriginTransform { get; set; }
+
+        public float ClippingSizeX { get; set; } = 1;
+
+        public float ClippingSizeZ { get; set; } = 1;
+
+        private void Start()
         {
-            return;
+            tileset = GetComponent<Cesium3DTileset>();
+            tileset.OnTileGameObjectCreated += Tileset_OnTileGameObjectCreated;
         }
 
-        var worldToLocal = ClippingOriginTransform.worldToLocalMatrix;
-
-        tiles.RemoveAll(x => x.TileObject == null);
-
-        foreach (var tile in tiles)
+        private void Tileset_OnTileGameObjectCreated(GameObject tileObject)
         {
-            if (tile.TileObject.activeInHierarchy == false)
+            var meshRenderers = tileObject.GetComponentsInChildren<MeshRenderer>();
+            var materials = meshRenderers.Select(x => x.material).ToArray();
+            tiles.Add((tileObject, materials));
+        }
+
+        private void LateUpdate()
+        {
+            if (ClippingOriginTransform == null)
             {
-                continue;
+                return;
             }
 
-            foreach (var tileMaterial in tile.Materials)
+            var worldToLocal = ClippingOriginTransform.worldToLocalMatrix;
+
+            tiles.RemoveAll(x => x.TileObject == null);
+
+            foreach (var tile in tiles)
             {
-                tileMaterial.SetMatrix("_ClippingOriginWorldToLocalMatrix", worldToLocal);
-                tileMaterial.SetFloat("_ClippingSizeX", ClippingSizeX / 2);
-                tileMaterial.SetFloat("_ClippingSizeZ", ClippingSizeZ / 2);
+                if (tile.TileObject.activeInHierarchy == false)
+                {
+                    continue;
+                }
+
+                foreach (var tileMaterial in tile.Materials)
+                {
+                    tileMaterial.SetMatrix("_ClippingOriginWorldToLocalMatrix", worldToLocal);
+                    tileMaterial.SetFloat("_ClippingSizeX", ClippingSizeX / 2);
+                    tileMaterial.SetFloat("_ClippingSizeZ", ClippingSizeZ / 2);
+                }
             }
         }
     }
