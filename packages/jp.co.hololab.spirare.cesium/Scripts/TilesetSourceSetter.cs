f490c733d1f78535659d00e808183362a965a02a
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2023-12-05 08:19:27 +0900

    update

--- packages/jp.co.hololab.spirare.cesium/Scripts/TilesetSourceSetter.cs
+++ packages/jp.co.hololab.spirare.cesium/Scripts/TilesetSourceSetter.cs
@@ -3,39 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using CesiumForUnity;
 
-#if UNITY_EDITOR
-using System.IO;
-using UnityEditor;
-#endif
-
 namespace HoloLab.Spirare.Cesium
 {
-    [ExecuteAlways]
     [RequireComponent(typeof(Cesium3DTileset))]
     public class TilesetSourceSetter : MonoBehaviour
     {
         [SerializeField]
         private TilesetSourceSettings settings;
 
-        private const string assetLoadPath = "Spirare/TilesetSourceSettings";
-        private static string AssetFilePathFromProjectRoot => $"Assets/Resources/{assetLoadPath}.asset";
-
         private void Awake()
         {
-            if (Application.isPlaying)
-            {
-                SetupTileset();
-            }
-
-#if UNITY_EDITOR
-            if (Application.isPlaying == false)
-            {
-                if (settings == null)
-                {
-                    SetDefaultSettingsAsset();
-                }
-            }
-#endif
+            SetupTileset();
         }
 
         private void SetupTileset()
@@ -49,35 +27,5 @@ namespace HoloLab.Spirare.Cesium
             var tileset = GetComponent<Cesium3DTileset>();
             tileset.url = settings.URL;
         }
-
-#if UNITY_EDITOR
-        private void SetDefaultSettingsAsset()
-        {
-            if (File.Exists(AssetFilePathFromProjectRoot))
-            {
-                var asset = Resources.Load<TilesetSourceSettings>(assetLoadPath);
-                settings = asset;
-            }
-            else
-            {
-                CreateParentDirectory(AssetFilePathFromProjectRoot);
-
-                var asset = ScriptableObject.CreateInstance<TilesetSourceSettings>();
-                AssetDatabase.CreateAsset(asset, AssetFilePathFromProjectRoot);
-                AssetDatabase.SaveAssets();
-
-                settings = asset;
-            }
-        }
-
-        private static void CreateParentDirectory(string path)
-        {
-            var directory = Path.GetDirectoryName(path);
-            if (Directory.Exists(directory) == false)
-            {
-                Directory.CreateDirectory(directory);
-            }
-        }
-#endif
     }
 }
