1e2fcf6eb2f43ee1df9675949a6ba074e8b7dc52
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2023-12-04 18:08:05 +0900

    update

--- unity/SpirareBrowser-Desktop-Cesium/Assets/App/Scripts/StartupAssetCopier.cs
+++ unity/SpirareBrowser-Desktop-Cesium/Assets/App/Scripts/StartupAssetCopier.cs
@@ -1,79 +1,83 @@
+#if UNITY_EDITOR
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-[InitializeOnLoad]
-internal class StartupAssetCopier
+namespace HoloLab.Spirare.Browser.Desktop.Cesium
 {
-    private static readonly string sessionKey = "_SpirareBrowserDesktopCesium_StartupAssetCopier";
-
-    private static readonly string sourcePath = "Assets/App/Resources~/GooglePhotorealistic3DTilesSourceSettings.asset";
-    private static readonly string sourceGuid = "";
-    private static readonly string destinationPath = "Assets/Resources/test/a/b/GooglePhotorealistic3DTilesSourceSettings.asset";
-
-    private static bool IsNewSession
+    [InitializeOnLoad]
+    internal class StartupAssetCopier
     {
-        get
+        private static readonly string sessionKey = "_SpirareBrowserDesktopCesium_StartupAssetCopier";
+
+        private static readonly string sourcePath = "Assets/App/Resources~/GooglePhotorealistic3DTilesSourceSettings.asset";
+        private static readonly string sourceGuid = "7f948eef0a404a64eb497bdaced032b5";
+        private static readonly string destinationPath = "Assets/Resources/Spirare/GooglePhotorealistic3DTilesSourceSettings.asset";
+
+        private static bool IsNewSession
         {
-            if (SessionState.GetBool(sessionKey, false))
+            get
+            {
+                if (SessionState.GetBool(sessionKey, false))
+                {
+                    return false;
+                }
+
+                SessionState.SetBool(sessionKey, true);
+                return true;
+            }
+        }
+
+        static StartupAssetCopier()
+        {
+            if (!IsNewSession || Application.isPlaying)
+            {
+                return;
+            }
+
+            CopyAssetUtility.TryCopyAsset(sourcePath, destinationPath, sourceGuid, copyMetaFile: true);
+        }
+    }
+
+    public static class CopyAssetUtility
+    {
+        public static void TryCopyAsset(string sourcePath, string destinationPath, string sourceGuid, bool copyMetaFile)
+        {
+            if (IsImported(sourceGuid))
+            {
+                return;
+            }
+
+            try
+            {
+                var destinationFolder = Path.GetDirectoryName(destinationPath);
+                Directory.CreateDirectory(destinationFolder);
+
+                File.Copy(sourcePath, destinationPath, false);
+                if (copyMetaFile)
+                {
+                    File.Copy(sourcePath + ".meta", destinationPath + ".meta", false);
+                }
+
+                AssetDatabase.Refresh();
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+
+        private static bool IsImported(string guid)
+        {
+            if (string.IsNullOrEmpty(guid))
             {
                 return false;
             }
 
-            SessionState.SetBool(sessionKey, true);
-            return true;
+            var path = AssetDatabase.GUIDToAssetPath(guid);
+            return !string.IsNullOrEmpty(path);
         }
-    }
-
-    static StartupAssetCopier()
-    {
-        if (!IsNewSession || Application.isPlaying)
-        {
-            // return;
-        }
-
-        TryCopyAsset();
-    }
-
-    private static void TryCopyAsset()
-    {
-        Debug.Log("Try copy asset");
-        if (IsImported(sourceGuid))
-        {
-            Debug.Log("Is imported");
-            return;
-        }
-
-        // copy source file to destination file
-
-        Debug.Log("Is not imported");
-
-        try
-        {
-
-            // Create dir with recursively
-            var destinationFolder = Path.GetDirectoryName(destinationPath);
-            Directory.CreateDirectory(destinationFolder);
-
-
-            File.Copy(sourcePath, destinationPath, false);
-
-            var sourceMetaPath = sourcePath + ".meta";
-            var destinationMetaPath = destinationPath + ".meta";
-            File.Copy(sourceMetaPath, destinationMetaPath, false);
-        }
-        catch (Exception ex)
-        {
-            Debug.LogException(ex);
-        }
-    }
-
-    private static bool IsImported(string guid)
-    {
-        var path = AssetDatabase.GUIDToAssetPath(guid);
-        return !string.IsNullOrEmpty(path);
     }
 }
+#endif
