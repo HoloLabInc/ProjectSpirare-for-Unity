adc9bdcd10dd40e3eccaa1fce51b783a9113ed3f
tarukosu <4415085+tarukosu@users.noreply.github.com>
2024-01-09 17:00:26 +0900

    Experimentally implement support for 3D Gaussian Splatting (#133)

---
+++ unity/SpirareBrowser-ARFoundation-Marker-Splat/Assets/App/SplatVFX/SplatModelElementComponent.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using System.Linq;
+using UnityEngine.VFX;
+
+namespace HoloLab.Spirare.Splat
+{
+    public sealed class SplatModelElementComponent : ModelElementComponent
+    {
+        private GameObject currentModelObject;
+        private string _currentModelSource;
+
+        private CameraVisibleHelper[] _cameraVisibleHelpers;
+
+        private VisualEffect splatPrefab;
+
+        private static readonly SplatVfxSplatLoader splatLoader = new SplatVfxSplatLoader();
+
+        private const string hiddenLayerName = "SpirareHidden";
+        private int HiddenLayer => ConvertLayerNameToLayer(hiddenLayerName);
+
+        public override WrapMode WrapMode
+        {
+            get
+            {
+                return WrapMode.Default;
+            }
+            set
+            {
+                return;
+            }
+        }
+
+        public void Initialize(PomlModelElement element, PomlLoadOptions loadOptions, VisualEffect splatPrefab)
+        {
+            Initialize(element, loadOptions);
+            this.splatPrefab = splatPrefab;
+        }
+
+        private async void OnEnable()
+        {
+            await ShowSplatModel();
+        }
+
+        public override bool IsWithinCamera(Camera camera)
+        {
+            return _cameraVisibleHelpers?.Any(x => x.IsInsideCameraBounds(camera)) ?? false;
+        }
+
+        protected override async Task UpdateGameObjectCore()
+        {
+            if (_currentModelSource == element.Src && currentDisplayType == DisplayType)
+            {
+                return;
+            }
+
+            if (currentModelObject != null)
+            {
+                Destroy(currentModelObject);
+                currentModelObject = null;
+                ChangeLoadingStatus(PomlElementLoadingStatus.NotLoaded);
+            }
+
+            currentDisplayType = DisplayType;
+
+            if (DisplayType != PomlDisplayType.Visible)
+            {
+                return;
+            }
+
+            _cameraVisibleHelpers = null;
+
+            var (success, splatObject) = await splatLoader.LoadAsync(transform, element.Src, splatPrefab);
+            currentModelObject = splatObject;
+
+            if (success)
+            {
+                await ShowSplatModel();
+            }
+
+            _currentModelSource = element.Src;
+        }
+
+        public override bool ChangeAnimation(int animationIndex)
+        {
+            return false;
+        }
+
+        public override bool ChangeAnimation(string animationName)
+        {
+            return false;
+        }
+
+        public override bool IsAnimationPlaying()
+        {
+            return false;
+        }
+
+        public override void PlayAnimation(WrapMode wrap)
+        {
+        }
+
+        public override void StopAnimation()
+        {
+        }
+
+        public override bool TryGetCurrentAnimation(out int index)
+        {
+            index = -1;
+            return false;
+        }
+
+        private async UniTask ShowSplatModel()
+        {
+            if (currentModelObject == null)
+            {
+                return;
+
+            }
+            currentModelObject.layer = HiddenLayer;
+            currentModelObject.transform.SetParent(null, worldPositionStays: false);
+
+            await UniTask.Yield();
+            await UniTask.Yield();
+
+            if (currentModelObject == null)
+            {
+                return;
+            }
+
+            currentModelObject.transform.SetParent(transform, worldPositionStays: false);
+            currentModelObject.layer = 0;
+        }
+
+        private static int ConvertLayerNameToLayer(string layerName)
+        {
+            var layer = LayerMask.NameToLayer(layerName);
+            if (layer == -1)
+            {
+                return 0;
+            }
+            else
+            {
+                return layer;
+            }
+        }
+    }
+}
---
+++ unity/SpirareBrowser-ARFoundation-Marker-Splat/Assets/App/SplatVFX/SplatModelElementObjectFactory.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace HoloLab.Spirare.Splat
+{
+    public class SplatModelElementObjectFactory : ModelElementObjectFactory
+    {
+        [SerializeField]
+        private VisualEffect splatPrefab;
+
+        public override GameObject CreateObject(PomlModelElement element, PomlLoadOptions loadOptions, Transform parentTransform = null)
+        {
+            var go = new GameObject("model");
+
+            if (parentTransform != null)
+            {
+                go.transform.SetParent(parentTransform, false);
+            }
+
+            var pomlObjectElementComponent = go.AddComponent<PomlObjectElementComponent>();
+            pomlObjectElementComponent.Initialize(element);
+
+            var modelElementComponent = go.AddComponent<SplatModelElementComponent>();
+            modelElementComponent.Initialize(element, loadOptions, splatPrefab);
+            _ = modelElementComponent.UpdateGameObject();
+
+            return go;
+        }
+    }
+}
