6ea945ee111c89b7ce90882cc80704ae25f1af04
tarukosu <4415085+tarukosu@users.noreply.github.com>
2024-01-31 14:31:29 +0900

    Enable pan zoom manipulation for Cesium 3D maps (#140)

---
+++ packages/jp.co.hololab.spirare.cesium3dmaps/Scripts/CesiumRectanbleMapManipulator.cs
@@ -0,0 +1,182 @@
+using HoloLab.PositioningTools.GeographicCoordinate;
+using Microsoft.MixedReality.Toolkit.Input;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace HoloLab.Spirare.Cesium3DMaps
+{
+    public class CesiumRectanbleMapManipulator : MonoBehaviour, IMixedRealityPointerHandler
+    {
+        private class PointerData
+        {
+            public Vector3 PreviousPosition;
+            public IMixedRealityPointer Pointer;
+            public readonly bool IsTargetPositionLockedOnFocusLock;
+
+            public PointerData(IMixedRealityPointer pointer, Vector3 previousPosition)
+            {
+                Pointer = pointer;
+                PreviousPosition = previousPosition;
+                IsTargetPositionLockedOnFocusLock = pointer.IsTargetPositionLockedOnFocusLock;
+            }
+        }
+
+        private CesiumRectangleMap cesiumRectangleMap;
+
+        private readonly List<PointerData> pointerDataList = new List<PointerData>();
+
+        private int PointerCount => pointerDataList.Count;
+
+        private void Start()
+        {
+            cesiumRectangleMap = GetComponentInParent<CesiumRectangleMap>();
+
+            var currentMapSize = (cesiumRectangleMap.MapSizeX, cesiumRectangleMap.MapSizeZ);
+            CesiumRectangleMap_OnMapSizeChanged(currentMapSize);
+
+            cesiumRectangleMap.OnMapSizeChanged += CesiumRectangleMap_OnMapSizeChanged;
+        }
+
+        private void CesiumRectangleMap_OnMapSizeChanged((float MapSizeX, float MapSizeZ) mapSize)
+        {
+            var localScale = transform.localScale;
+            localScale.x = mapSize.MapSizeX;
+            localScale.y = mapSize.MapSizeZ;
+            transform.localScale = localScale;
+        }
+
+        public void OnPointerDown(MixedRealityPointerEventData eventData)
+        {
+            if (TryGetPointerData(eventData.Pointer.PointerId, out _))
+            {
+                return;
+            }
+
+            var pointer = eventData.Pointer;
+            var pointerLocalPosition = GetLocalPosition(pointer.Result.Details.Point);
+            var pointerData = new PointerData(pointer, pointerLocalPosition);
+            pointer.IsTargetPositionLockedOnFocusLock = false;
+
+            pointerDataList.Add(pointerData);
+
+            eventData.Use();
+        }
+
+        public void OnPointerDragged(MixedRealityPointerEventData eventData)
+        {
+            var pointer = eventData.Pointer;
+            if (TryGetPointerData(pointer.PointerId, out var pointerData) == false)
+            {
+                return;
+            }
+
+            var pointerLocalPosition = GetPointerLocalPosition(pointer);
+            var deltaPosition = pointerLocalPosition - pointerData.PreviousPosition;
+
+            ChangeMapCenter(deltaPosition);
+
+            if (PointerCount >= 2)
+            {
+                ChangeMapScale(pointerData, pointerLocalPosition);
+            }
+
+            pointerData.PreviousPosition = pointerLocalPosition;
+        }
+
+        public void OnPointerUp(MixedRealityPointerEventData eventData)
+        {
+            var pointer = eventData.Pointer;
+
+            if (TryGetPointerData(pointer.PointerId, out var pointerData) == false)
+            {
+                return;
+            }
+
+            pointer.IsTargetPositionLockedOnFocusLock = pointerData.IsTargetPositionLockedOnFocusLock;
+            pointerDataList.Remove(pointerData);
+
+            eventData.Use();
+        }
+
+        public void OnPointerClicked(MixedRealityPointerEventData eventData)
+        {
+            // Do nothing
+        }
+
+        private Vector3 GetPointerLocalPosition(IMixedRealityPointer pointer)
+        {
+            var pointerLocalPosition = GetLocalPosition(pointer.Result.Details.Point);
+
+            if (pointer.Result.Details.LastRaycastHit.transform == transform)
+            {
+                return pointerLocalPosition;
+            }
+            else
+            {
+                var startPoint = GetLocalPosition(pointer.Result.StartPoint);
+
+                var t = Mathf.InverseLerp(startPoint.z, pointerLocalPosition.z, 0);
+                var pointerLocalPositionIntersectingOperatingPlane = Vector3.Lerp(startPoint, pointerLocalPosition, t);
+                return pointerLocalPositionIntersectingOperatingPlane;
+            }
+        }
+
+        private Vector3 GetLocalPosition(Vector3 worldPosition)
+        {
+            var localPoint = transform.InverseTransformPoint(worldPosition);
+            return localPoint;
+        }
+
+        private bool TryGetPointerData(uint pointerId, out PointerData pointerData)
+        {
+            pointerData = pointerDataList.FirstOrDefault(x => x.Pointer.PointerId == pointerId);
+            return pointerData != null;
+        }
+
+        private void ChangeMapCenter(Vector3 deltaPosition)
+        {
+            var panDelta = deltaPosition / PointerCount;
+
+            var newCenterEnu = ConvertLocalPositionToEnuPosition(new Vector3(-panDelta.x, -panDelta.y, 0));
+            var newCenterPosition = cesiumRectangleMap.ConvertEnuPositionToGeodeticPosition(newCenterEnu);
+
+            var currentCenter = cesiumRectangleMap.Center;
+            cesiumRectangleMap.Center = new GeodeticPosition(newCenterPosition.Latitude, newCenterPosition.Longitude, currentCenter.EllipsoidalHeight);
+        }
+
+        private void ChangeMapScale(PointerData pointerData, Vector3 pointerLocalPosition)
+        {
+            var otherPointersSum = pointerDataList
+                .Where(x => x != pointerData)
+                .Aggregate(Vector3.zero, (s, x) => s + x.PreviousPosition);
+
+            var previousCenter = (otherPointersSum + pointerData.PreviousPosition) / PointerCount;
+            var currentCenter = (otherPointersSum + pointerLocalPosition) / PointerCount;
+
+            // To prevent sudden changes in scale, use threshold for pointer distance.
+            var distanceThreshold = 0.01f;
+            var previousDistance = Mathf.Max((previousCenter - pointerData.PreviousPosition).magnitude, distanceThreshold);
+            var currentDistance = Mathf.Max((currentCenter - pointerLocalPosition).magnitude, distanceThreshold);
+
+            var mapScale = cesiumRectangleMap.Scale / previousDistance * currentDistance;
+            var scaleCenter = (previousCenter + currentCenter) / 2;
+
+            var scaleCenterEnu = ConvertLocalPositionToEnuPosition(scaleCenter);
+            cesiumRectangleMap.ScaleAroundEnuPosition(mapScale, scaleCenterEnu);
+        }
+
+        private EnuPosition ConvertLocalPositionToEnuPosition(Vector3 localPosition)
+        {
+            var mapScale = cesiumRectangleMap.Scale;
+
+            var east = localPosition.x * cesiumRectangleMap.MapSizeX / mapScale;
+            var north = localPosition.y * cesiumRectangleMap.MapSizeZ / mapScale;
+            var up = localPosition.z / mapScale;
+
+            return new EnuPosition(east, north, up);
+        }
+    }
+}
--- packages/jp.co.hololab.spirare.cesium3dmaps/Scripts/CesiumRectangleMap.cs
+++ packages/jp.co.hololab.spirare.cesium3dmaps/Scripts/CesiumRectangleMap.cs
@@ -5,8 +5,6 @@ using HoloLab.Spirare.Cesium;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.Mathematics;
 using UnityEngine;
 
 namespace HoloLab.Spirare.Cesium3DMaps
@@ -58,12 +56,18 @@ namespace HoloLab.Spirare.Cesium3DMaps
             }
             set
             {
-                scale = Mathf.Max(value, MinimumScale);
+                scale = Mathf.Clamp(value, scaleMin, scaleMax);
                 SaveScale();
                 UpdateMap();
                 InvokeOnScaleChanged(scale);
             }
         }
+
+        [SerializeField]
+        private float scaleMin = 1 / 1_000_000f;
+
+        [SerializeField]
+        private float scaleMax = 1;
 
         [SerializeField]
         private GeodeticPositionForInspector center;
@@ -92,8 +96,6 @@ namespace HoloLab.Spirare.Cesium3DMaps
         private CesiumGeoreference[] cesiumGeoreferences;
         private CesiumGeodeticAreaExcluder[] cesiumGeodeticAreaExcluders;
 
-        private const float MinimumScale = 0.000001f;
-
         private const string PlayerPrefs_CenterKey = "CesiumRectangleMap_Center";
         private const string PlayerPrefs_ScaleKey = "CesiumRectangleMap_Scale";
 
@@ -115,6 +117,29 @@ namespace HoloLab.Spirare.Cesium3DMaps
             LoadScale();
 
             UpdateMap();
+        }
+
+        public GeodeticPosition ConvertEnuPositionToGeodeticPosition(EnuPosition enuPosition)
+        {
+            return EnuToGeodetic(enuPosition);
+        }
+
+        public void ScaleAroundEnuPosition(float mapScale, EnuPosition scaleCenter)
+        {
+            var scaleCenterGeodetic = EnuToGeodetic(scaleCenter, Center);
+            var scaleCenterToCurrentMapCenter = GeodeticToEnu(Center, scaleCenterGeodetic);
+
+            var relativeScale = (double)Scale / mapScale;
+            var scaleCenterToNewMapCenter = new EnuPosition(
+                scaleCenterToCurrentMapCenter.East * relativeScale,
+                scaleCenterToCurrentMapCenter.North * relativeScale,
+                scaleCenterToCurrentMapCenter.Up * relativeScale);
+
+            var newMapCenter = EnuToGeodetic(scaleCenterToNewMapCenter, scaleCenterGeodetic);
+            var newMapCenterWithSameHeight = new GeodeticPosition(newMapCenter.Latitude, newMapCenter.Longitude, Center.EllipsoidalHeight);
+
+            Center = newMapCenterWithSameHeight;
+            Scale = mapScale;
         }
 
         private void AttachTilesetClipperForChildTilesets()
@@ -192,22 +217,16 @@ namespace HoloLab.Spirare.Cesium3DMaps
 
         private void UpdateCesiumGeodeticAreaExcluders()
         {
-            var georeference = cesiumGeoreferences.FirstOrDefault();
-            if (georeference == null)
-            {
-                return;
-            }
-
-            var upperLeftLonLatHeight = EnuToGeodetic(georeference, new double3(-mapSizeX / 2 / scale, 0, mapSizeZ / 2 / scale));
-            var lowerRightLonLatHeight = EnuToGeodetic(georeference, new double3(mapSizeX / 2 / scale, 0, -mapSizeZ / 2 / scale));
+            var upperLeftLonLatHeight = EnuToGeodetic(new EnuPosition(-mapSizeX / 2 / scale, mapSizeZ / 2 / scale, 0));
+            var lowerRightLonLatHeight = EnuToGeodetic(new EnuPosition(mapSizeX / 2 / scale, -mapSizeZ / 2 / scale, 0));
 
             foreach (var cesiumGeodeticAreaExcluder in cesiumGeodeticAreaExcluders)
             {
-                cesiumGeodeticAreaExcluder.UpperLeftLongitude = upperLeftLonLatHeight.x;
-                cesiumGeodeticAreaExcluder.UpperLeftLatitude = upperLeftLonLatHeight.y;
+                cesiumGeodeticAreaExcluder.UpperLeftLongitude = upperLeftLonLatHeight.Longitude;
+                cesiumGeodeticAreaExcluder.UpperLeftLatitude = upperLeftLonLatHeight.Latitude;
 
-                cesiumGeodeticAreaExcluder.LowerRightLongitude = lowerRightLonLatHeight.x;
-                cesiumGeodeticAreaExcluder.LowerRightLatitude = lowerRightLonLatHeight.y;
+                cesiumGeodeticAreaExcluder.LowerRightLongitude = lowerRightLonLatHeight.Longitude;
+                cesiumGeodeticAreaExcluder.LowerRightLatitude = lowerRightLonLatHeight.Latitude;
             }
         }
 
@@ -247,11 +266,19 @@ namespace HoloLab.Spirare.Cesium3DMaps
             }
         }
 
-        private static double3 EnuToGeodetic(CesiumGeoreference georeference, double3 enuPosition)
+        private GeodeticPosition EnuToGeodetic(EnuPosition enuPosition)
         {
-            var ecef = georeference.TransformUnityPositionToEarthCenteredEarthFixed(enuPosition);
-            var lonLatHeight = CesiumWgs84Ellipsoid.EarthCenteredEarthFixedToLongitudeLatitudeHeight(ecef);
-            return lonLatHeight;
+            return EnuToGeodetic(enuPosition, Center);
+        }
+
+        private static GeodeticPosition EnuToGeodetic(EnuPosition enuPosition, GeodeticPosition originPosition)
+        {
+            return GeographicCoordinateConversion.EnuToGeodetic(enuPosition, originPosition);
+        }
+
+        private static EnuPosition GeodeticToEnu(GeodeticPosition geodeticPosition, GeodeticPosition originPosition)
+        {
+            return GeographicCoordinateConversion.GeodeticToEnu(geodeticPosition, originPosition);
         }
     }
 }
