97dd42026cda6128252c37a8d2546cad83e63010
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2024-02-05 15:05:17 +0900

    update

--- packages/jp.co.hololab.spirare.cesium/Scripts/Cesium3dTilesElementComponentFactory.cs
+++ packages/jp.co.hololab.spirare.cesium/Scripts/Cesium3dTilesElementComponentFactory.cs
@@ -86,17 +86,16 @@ namespace HoloLab.Spirare.Cesium
             var url = cesium3dTilesElement.Src;
 
             Debug.Log(url);
-            // Remove file:// from url
             if (url.StartsWith("file://"))
             {
+                // Remove file:// from url
                 var path = url.Substring(7);
                 Debug.Log("path: " + path);
 
-                // get directory from path
                 var directoryPath = Path.GetDirectoryName(path);
                 var fileName = Path.GetFileName(path);
 
-                var localServerUrl = $"http://localhost:{localFileServer.Port}/{fileName}?path={directoryPath}";
+                var localServerUrl = $"http://localhost:{localFileServer.Port}/{fileName}?basepath={directoryPath}";
                 return localServerUrl;
             }
 
@@ -107,111 +106,6 @@ namespace HoloLab.Spirare.Cesium
             url = url.Replace(" ", "%20");
             */
             return url;
-        }
-    }
-
-    internal class LocalFileServer : IDisposable
-    {
-        private readonly HttpServer httpServer = new HttpServer();
-
-        private int port;
-
-        public int Port => port;
-
-        public void StartOnRandomPort()
-        {
-            port = 12345;
-            httpServer.Start(port);
-
-            httpServer.OnRequest += HttpServer_OnRequest;
-        }
-
-        public void Dispose()
-        {
-            httpServer.Stop();
-        }
-
-        private async void HttpServer_OnRequest(HttpListenerContext context)
-        {
-            var request = context.Request;
-            var response = context.Response;
-
-            // get path query from request
-            var query = request.Url.Query;
-            //request.QueryString["aa"]
-
-            var pathKey = "path";
-            if (request.QueryString.AllKeys.Contains(pathKey) == false)
-            {
-                response.StatusCode = 400;
-                response.Close();
-                return;
-            }
-
-            var basePath = request.QueryString[pathKey];
-
-            Debug.LogWarning(request.RawUrl);
-            Debug.LogWarning(request.Url.AbsolutePath);
-            Debug.LogWarning(basePath);
-            // TODO return file
-
-            // Remove slash from url path
-            var relativePath = request.Url.AbsolutePath.Substring(1);
-            var filePath = Path.Combine(basePath, relativePath);
-            Debug.LogWarning(filePath);
-
-            if (response.OutputStream.CanWrite)
-            {
-                try
-                {
-                    using (var fileStream = File.OpenRead(filePath))
-                    {
-                        await fileStream.CopyToAsync(response.OutputStream);
-                        Debug.LogWarning(fileStream.Length);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Debug.LogException(ex);
-                    response.StatusCode = 404;
-                }
-            }
-
-            response.Close();
-        }
-    }
-
-    internal class HttpServer
-    {
-        private HttpListener httpListener;
-        private CancellationTokenSource tokenSource;
-
-        public event Action<HttpListenerContext> OnRequest;
-
-        public void Start(int port)
-        {
-            httpListener = new HttpListener();
-            var uri = $"http://*:{port}/";
-            httpListener.Prefixes.Add(uri);
-
-            tokenSource = new CancellationTokenSource();
-            var token = tokenSource.Token;
-
-            Task.Run(async () =>
-            {
-                httpListener.Start();
-                while (true)
-                {
-                    var context = await httpListener.GetContextAsync();
-                    OnRequest?.Invoke(context);
-                }
-            }, token);
-        }
-
-        public void Stop()
-        {
-            tokenSource?.Cancel();
-            httpListener?.Stop();
         }
     }
 }
