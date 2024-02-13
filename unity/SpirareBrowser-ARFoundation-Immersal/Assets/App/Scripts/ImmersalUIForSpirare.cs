2d9675ca0f271dcf873cf2833f568e27528d7955
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2024-02-13 11:31:33 +0900

    update

---
+++ unity/SpirareBrowser-ARFoundation-Immersal/Assets/App/Scripts/ImmersalUIForSpirare.cs
@@ -0,0 +1,52 @@
+using HoloLab.PositioningTools.Immersal;
+using HoloLab.PositioningTools.Immersal.UI;
+using HoloLab.Spirare.Browser.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HoloLab.Spirare.Browser.Immersal
+{
+    public class ImmersalUIForSpirare : MonoBehaviour
+    {
+        [SerializeField]
+        private ImmersalMapManager immersalMapManager = null;
+
+        private ImmersalSignInUI immersalSignInUI;
+
+        private const string emailAddressKey = "ImmersalUIForSpirare_EmailAddress";
+        private const string passwordKey = "ImmersalUIForSpirare_Password";
+
+        private void Start()
+        {
+            immersalSignInUI = GetComponentInChildren<ImmersalSignInUI>();
+            LoadUserInfo();
+
+            immersalMapManager.OnLogin += ImmersalMapManager_OnLogin;
+        }
+
+        private void ImmersalMapManager_OnLogin()
+        {
+            SaveUserInfo();
+        }
+
+        private void LoadUserInfo()
+        {
+            if (EncryptedPrefs.LoadString(emailAddressKey, out var emailAddress))
+            {
+                immersalSignInUI.EmailAddress = emailAddress;
+            }
+
+            if (EncryptedPrefs.LoadString(passwordKey, out var password))
+            {
+                immersalSignInUI.Password = password;
+            }
+        }
+
+        private void SaveUserInfo()
+        {
+            EncryptedPrefs.SaveString(emailAddressKey, immersalSignInUI.EmailAddress);
+            EncryptedPrefs.SaveString(passwordKey, immersalSignInUI.Password);
+        }
+    }
+}
