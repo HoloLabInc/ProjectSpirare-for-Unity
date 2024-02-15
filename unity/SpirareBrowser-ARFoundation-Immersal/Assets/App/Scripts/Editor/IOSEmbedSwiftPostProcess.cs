bddc39ee51c58b411fd6e9b1d8bb4acd2f3a4297
tarukosu <4415085+tarukosu@users.noreply.github.com>
2024-02-15 11:31:53 +0900

    [SpirareBrowser-ARFoundation-Immersal] Fix the issue of failing to archive in Xcode (#162)

---
+++ unity/SpirareBrowser-ARFoundation-Immersal/Assets/App/Scripts/Editor/IOSEmbedSwiftPostProcess.cs
@@ -0,0 +1,36 @@
+#if UNITY_EDITOR && UNITY_IOS
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEditor.iOS.Xcode;
+
+namespace HoloLab.Spirare.Browser.Immersal.Editor
+{
+    public class IOSEmbedSwiftPostProcess : IPostprocessBuildWithReport
+    {
+        public int callbackOrder => 0;
+
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            if (report.summary.platformGroup == UnityEditor.BuildTargetGroup.iOS)
+            {
+                var projectPath = PBXProject.GetPBXProjectPath(report.summary.outputPath);
+
+                var project = new PBXProject();
+                project.ReadFromFile(projectPath);
+
+                var mainTargetGuid = project.GetUnityMainTargetGuid();
+                var frameworkTargetGuid = project.GetUnityFrameworkTargetGuid();
+
+                var key = "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES";
+                project.SetBuildProperty(mainTargetGuid, key, "YES");
+                project.SetBuildProperty(frameworkTargetGuid, key, "NO");
+
+                project.WriteToFile(projectPath);
+            }
+        }
+    }
+}
+#endif
