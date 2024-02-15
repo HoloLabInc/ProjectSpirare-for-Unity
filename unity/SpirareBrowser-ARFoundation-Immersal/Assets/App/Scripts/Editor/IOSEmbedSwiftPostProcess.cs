68a1e2c2cf75de64805da948a26201295d12814a
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2024-02-15 11:00:34 +0900

    update

--- unity/SpirareBrowser-ARFoundation-Immersal/Assets/App/Scripts/Editor/IOSEmbedSwiftPostProcess.cs
+++ unity/SpirareBrowser-ARFoundation-Immersal/Assets/App/Scripts/Editor/IOSEmbedSwiftPostProcess.cs
@@ -1,12 +1,14 @@
 #if UNITY_EDITOR && UNITY_IOS
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEditor.iOS.Xcode;
 
-namespace HoloLab.Spirare.Browser.Immersal.Editor{
-public  class IOSEmbedSwiftPostProcess : IPostprocessBuildWithReport
+namespace HoloLab.Spirare.Browser.Immersal.Editor
+{
+    public class IOSEmbedSwiftPostProcess : IPostprocessBuildWithReport
     {
         public int callbackOrder => 0;
 
@@ -14,19 +16,19 @@ public  class IOSEmbedSwiftPostProcess : IPostprocessBuildWithReport
         {
             if (report.summary.platformGroup == UnityEditor.BuildTargetGroup.iOS)
             {
-                var projectPath =   PBXProject.GetPBXProjectPath(report.summary.outputPath);
+                var projectPath = PBXProject.GetPBXProjectPath(report.summary.outputPath);
 
                 var project = new PBXProject();
                 project.ReadFromFile(projectPath);
 
-                var     mainTargetGuid =    project.GetUnityMainTargetGuid();
+                var mainTargetGuid = project.GetUnityMainTargetGuid();
                 var frameworkTargetGuid = project.GetUnityFrameworkTargetGuid();
 
-                    var key = "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES";
-               project.SetBuildProperty(frameworkTargetGuid, key, "NO");
+                var key = "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES";
+                project.SetBuildProperty(mainTargetGuid, key, "YES");
+                project.SetBuildProperty(frameworkTargetGuid, key, "NO");
 
-               project.SetBuildProperty(mainTargetGuid, key, "YES");
-               project.WriteToFile(projectPath);
+                project.WriteToFile(projectPath);
             }
         }
     }
