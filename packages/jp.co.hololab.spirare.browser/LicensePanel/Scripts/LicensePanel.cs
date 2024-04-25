a7e005922bfb91d68c7b7102302308f76460bb6d
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2024-04-25 16:35:05 +0900

    update

---
+++ packages/jp.co.hololab.spirare.browser/LicensePanel/Scripts/LicensePanel.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HoloLab.Spirare.Browser
+{
+    public class LicensePanel : MonoBehaviour
+    {
+        [SerializeField]
+        private LicensePanelState licensePanelState;
+
+        [SerializeField]
+        private Button closeButton;
+
+        [SerializeField]
+        private TMP_Text licenseText;
+
+        [SerializeField]
+        private TextAsset licenseTextAsset;
+
+        private void Start()
+        {
+            licenseText.text = licenseTextAsset.text;
+
+            LicensePanelState_OnIsPanelOpenChanged();
+            licensePanelState.OnIsPanelOpenChanged += LicensePanelState_OnIsPanelOpenChanged;
+        }
+
+        private void LicensePanelState_OnIsPanelOpenChanged()
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(licensePanelState.IsPanelOpen);
+            }
+        }
+    }
+}
+
