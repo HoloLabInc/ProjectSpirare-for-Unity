9f0226725828b2101c89dd0366f530e96b645dac
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2024-05-07 14:56:15 +0900

    update

--- packages/jp.co.hololab.spirare.browser/UI/SideMenu/Scripts/ShowLicenseButton.cs
+++ packages/jp.co.hololab.spirare.browser/UI/SideMenu/Scripts/ShowLicenseButton.cs
@@ -2,33 +2,79 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace HoloLab.Spirare.Browser.UI
 {
-    public class ShowLicenseButton : MonoBehaviour
+    public class ShowLicenseButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, IDragHandler
     {
         [SerializeField]
         private LicensePanelState licensePanelState;
 
-        private Button button;
+        [SerializeField]
+        private DeveloperModeState developerModeState;
+
+        private float? pointerDownTime;
+
+        private float longTapDuration = 2.0f;
 
         private SideMenu sideMenu;
+
 
         private void Awake()
         {
             sideMenu = GetComponentInParent<SideMenu>();
-            button = GetComponent<Button>();
-            button.onClick.AddListener(OnClick);
         }
 
-        private void OnClick()
+        private void Update()
+        {
+            if (pointerDownTime.HasValue)
+            {
+                if (Time.realtimeSinceStartup - pointerDownTime > longTapDuration)
+                {
+                    ToggleDeveloperMode();
+                    pointerDownTime = null;
+                }
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            pointerDownTime = Time.realtimeSinceStartup;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (pointerDownTime.HasValue)
+            {
+                ShowLicense();
+            }
+
+            pointerDownTime = null;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            pointerDownTime = null;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            // Do nothing
+        }
+
+        private void ShowLicense()
         {
             licensePanelState.IsPanelOpen = true;
             if (sideMenu != null)
             {
                 sideMenu.CloseMenu();
             }
+        }
+
+        private void ToggleDeveloperMode()
+        {
+            developerModeState.Enabled = !developerModeState.Enabled;
         }
     }
 }
