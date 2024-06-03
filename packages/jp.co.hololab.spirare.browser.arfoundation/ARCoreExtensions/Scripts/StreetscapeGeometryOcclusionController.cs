f35570578a232b609726bd220a5e532c42564114
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2024-06-03 09:39:26 +0900

    update

--- packages/jp.co.hololab.spirare.browser.arfoundation/ARCoreExtensions/Scripts/StreetscapeGeometryOcclusionController.cs
+++ packages/jp.co.hololab.spirare.browser.arfoundation/ARCoreExtensions/Scripts/StreetscapeGeometryOcclusionController.cs
@@ -2,32 +2,42 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static HoloLab.Spirare.Browser.DisplaySettingsState;
 
 namespace HoloLab.Spirare.Browser.ARFoundation.ARCoreExtensions
 {
     public class StreetscapeGeometryOcclusionController : MonoBehaviour
     {
         [SerializeField]
-        private GameObject streetscapeGeometryController;
+        private StreetscapeGeometryController streetscapeGeometryController;
 
         [SerializeField]
         private DisplaySettingsState displaySettingsState;
 
         private void Start()
         {
-            SetStreetscapeGeometryControllerActive(displaySettingsState.StreetscapeGeometryOcclusionEnabled);
+            SetStreetscapeGeometryControllerOcclusionType(displaySettingsState.StreetscapeGeometryOcclusion);
 
-            displaySettingsState.OnStreetscapeGeometryOcclusionEnabledChanged += DisplaySettingsState_OnStreetscapeGeometryOcclusionEnabledChanged;
+            displaySettingsState.OnStreetscapeGeometryOcclusionChanged += DisplaySettingsState_OnStreetscapeGeometryOcclusionChanged;
         }
 
-        private void DisplaySettingsState_OnStreetscapeGeometryOcclusionEnabledChanged(bool enabled)
+        private void DisplaySettingsState_OnStreetscapeGeometryOcclusionChanged(StreetscapeGeometryOcclusionType occlusionType)
         {
-            SetStreetscapeGeometryControllerActive(enabled);
+            SetStreetscapeGeometryControllerOcclusionType(occlusionType);
         }
 
-        private void SetStreetscapeGeometryControllerActive(bool enabled)
+        private void SetStreetscapeGeometryControllerOcclusionType(StreetscapeGeometryOcclusionType occlusionType)
         {
-            streetscapeGeometryController.SetActive(enabled);
+            var controllerOcclusionType = occlusionType switch
+            {
+                StreetscapeGeometryOcclusionType.None => StreetscapeGeometryController.StreetscapeGeometryOcclusionType.None,
+                StreetscapeGeometryOcclusionType.Building => StreetscapeGeometryController.StreetscapeGeometryOcclusionType.Building,
+                StreetscapeGeometryOcclusionType.Terrain => StreetscapeGeometryController.StreetscapeGeometryOcclusionType.Terrain,
+                StreetscapeGeometryOcclusionType.All => StreetscapeGeometryController.StreetscapeGeometryOcclusionType.All,
+                _ => StreetscapeGeometryController.StreetscapeGeometryOcclusionType.None
+            };
+
+            streetscapeGeometryController.SetOcclusionType(controllerOcclusionType);
         }
     }
 }
