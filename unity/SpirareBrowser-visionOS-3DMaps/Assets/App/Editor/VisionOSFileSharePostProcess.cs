adb18eda0e8164d1d3509a0a359419657c1a2299
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2024-07-11 11:33:40 +0900

    Update

---
+++ unity/SpirareBrowser-visionOS-3DMaps/Assets/App/Editor/VisionOSFileSharePostProcess.cs
@@ -0,0 +1,34 @@
+#if UNITY_EDITOR && UNITY_VISIONOS
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEditor.iOS.Xcode;
+
+namespace HoloLab.Spirare.Browser.VisionOS.Maps.Editor
+{
+    public class VisionOSFileSharePostProcess : IPostprocessBuildWithReport
+    {
+        public int callbackOrder => 0;
+
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            if (report.summary.platformGroup == UnityEditor.BuildTargetGroup.VisionOS)
+            {
+                var plistPath = Path.Combine(report.summary.outputPath, "Info.plist");
+                var plist = new PlistDocument();
+                plist.ReadFromFile(plistPath);
+
+                // Enable file sharing using iTunes.
+                plist.root.SetBoolean("UIFileSharingEnabled", true);
+
+                // Enable the display of the save folder in the Files app.
+                plist.root.SetBoolean("LSSupportsOpeningDocumentsInPlace", true);
+
+                plist.WriteToFile(plistPath);
+            }
+        }
+    }
+}
+#endif
