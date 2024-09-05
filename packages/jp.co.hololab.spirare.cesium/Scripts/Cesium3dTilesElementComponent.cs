32d071934eeffddf6c14f61908b9998bc731a804
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2024-09-05 09:09:46 +0900

    update

--- packages/jp.co.hololab.spirare.cesium/Scripts/Cesium3dTilesElementComponent.cs
+++ packages/jp.co.hololab.spirare.cesium/Scripts/Cesium3dTilesElementComponent.cs
@@ -1,9 +1,15 @@
+#if PRESENT_CESIUM_CARTOGRAPHIC_POLYGON && PRESENT_SPLINES
+#define CESIUM_CARTOGRAPHIC_POLYGON_ENABLED
+#endif
+
 using CesiumForUnity;
 using HoloLab.PositioningTools.CoordinateSystem;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Splines;
 
 namespace HoloLab.Spirare.Cesium
 {
@@ -11,6 +17,12 @@ namespace HoloLab.Spirare.Cesium
     public class Cesium3dTilesElementComponent : SpecificObjectElementComponentBase<PomlCesium3dTilesElement>
     {
         private Cesium3DTileset tileset;
+
+#if CESIUM_CARTOGRAPHIC_POLYGON_ENABLED
+        private List<CesiumCartographicPolygon> cesiumCartographicPolygons = new List<CesiumCartographicPolygon>();
+        private GameObject maskObject;
+        private CesiumPolygonRasterOverlay cesiumPolygonRasterOverlay;
+#endif
 
         private static LocalFileServer localFileServer;
 
@@ -29,7 +41,7 @@ namespace HoloLab.Spirare.Cesium
                 tileset.url = tilesetUrl;
             }
 
-            // TODO create mask
+            UpdateMask();
 
             return Task.CompletedTask;
         }
@@ -48,6 +60,79 @@ namespace HoloLab.Spirare.Cesium
                 localFileServer.StartOnRandomPort();
             }
         }
+
+        private void UpdateMask()
+        {
+#if CESIUM_CARTOGRAPHIC_POLYGON_ENABLED
+            ClearMask();
+
+            var masks = element.Masks;
+            if (masks.Count == 0)
+            {
+                if (cesiumPolygonRasterOverlay != null)
+                {
+                    Destroy(cesiumPolygonRasterOverlay);
+                }
+                return;
+            }
+
+            var mask = masks[0];
+            CreateMaskObjects(mask);
+            UpdatePolygonRasterOverlay();
+#endif
+        }
+
+#if CESIUM_CARTOGRAPHIC_POLYGON_ENABLED
+        private void ClearMask()
+        {
+            if (maskObject != null)
+            {
+                Destroy(maskObject);
+            }
+
+            maskObject = null;
+            cesiumCartographicPolygons.Clear();
+
+            if (cesiumPolygonRasterOverlay != null)
+            {
+                cesiumPolygonRasterOverlay.polygons.Clear();
+            }
+        }
+
+        private void CreateMaskObjects(PomlCesium3dTilesMask mask)
+        {
+            maskObject = new GameObject("Mask");
+            maskObject.transform.SetParent(transform, false);
+
+            foreach (var bounds in mask.Bounds)
+            {
+                var cesiumCartographicPolygon = CreateCesiumCartographicPolygon("Bounds", maskObject.transform, bounds);
+                cesiumCartographicPolygons.Add(cesiumCartographicPolygon);
+            }
+        }
+
+        private void UpdatePolygonRasterOverlay()
+        {
+            if (cesiumPolygonRasterOverlay == null)
+            {
+                cesiumPolygonRasterOverlay = tileset.gameObject.AddComponent<CesiumPolygonRasterOverlay>();
+            }
+
+            var polygons = cesiumPolygonRasterOverlay.polygons;
+            polygons.AddRange(cesiumCartographicPolygons);
+        }
+
+        private CesiumCartographicPolygon CreateCesiumCartographicPolygon(string name, Transform parent, PomlCesium3dTilesMaskBounds bounds)
+        {
+            var boundsObject = new GameObject(name);
+            boundsObject.transform.SetParent(parent, false);
+
+            var splineContainer = boundsObject.AddComponent<SplineContainer>();
+            var cesiumGlobeAnchor = boundsObject.AddComponent<CesiumGlobeAnchor>();
+            var cesiumCartographicPolygon = boundsObject.AddComponent<CesiumCartographicPolygon>();
+            return cesiumCartographicPolygon;
+        }
+#endif
 
         private static string GetTilesetUrl(PomlCesium3dTilesElement cesium3dTilesElement)
         {
