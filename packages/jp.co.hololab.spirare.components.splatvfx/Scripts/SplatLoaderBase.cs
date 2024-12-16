ccef47ffee9339e5a974ea3389e0af78df85ef7a
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2024-12-16 11:21:45 +0900

    update

---
+++ packages/jp.co.hololab.spirare.components.splatvfx/Scripts/SplatLoaderBase.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System;
+using Cysharp.Threading.Tasks;
+
+namespace HoloLab.Spirare.Components.SplatVfx
+{
+    internal abstract class SplatLoaderBase
+    {
+        public enum LoadingStatus
+        {
+            None,
+            DataFetching,
+            ModelLoading,
+            ModelInstantiating,
+            Loaded,
+            DataFetchError,
+            ModelLoadError,
+            ModelInstantiateError
+        }
+
+        internal static async UniTask<(bool Success, byte[] Data)> FetchData(string src, Action<LoadingStatus> onLoadingStatusChanged)
+        {
+            InvokeLoadingStatusChanged(LoadingStatus.DataFetching, onLoadingStatusChanged);
+
+            var result = await SpirareHttpClient.Instance.GetByteArrayAsync(src, enableCache: true);
+            if (result.Success)
+            {
+                return (true, result.Data);
+            }
+            else
+            {
+                InvokeLoadingStatusChanged(LoadingStatus.DataFetchError, onLoadingStatusChanged);
+                Debug.LogWarning($"Failed to get model data: {src}");
+
+                return (false, null);
+            }
+        }
+
+        internal static void InvokeLoadingStatusChanged(LoadingStatus status, Action<LoadingStatus> onLoadingStatusChanged)
+        {
+            try
+            {
+                onLoadingStatusChanged?.Invoke(status);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+    }
+}
+
