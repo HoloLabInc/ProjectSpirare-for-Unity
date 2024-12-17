5844ac4ac1a16f44abea1f644b27bbe933592b41
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2024-12-17 14:18:37 +0900

    update

--- packages/jp.co.hololab.spirare.components.splatvfx/Scripts/SplatVfxSpzLoader.cs
+++ packages/jp.co.hololab.spirare.components.splatvfx/Scripts/SplatVfxSpzLoader.cs
@@ -2,11 +2,7 @@
 using UnityEngine;
 using System.Threading;
 using System;
-using Cysharp.Threading.Tasks;
 using SplatVfx;
-using System.Runtime.InteropServices;
-using Unity.Burst;
-using Unity.Mathematics;
 using UnityEngine.VFX;
 using System.IO.Compression;
 using System.IO;
@@ -34,7 +30,7 @@ namespace HoloLab.Spirare.Components.SplatVfx
                 return (false, null);
             }
 
-            var result = TryCreateSplatData(fetchResult.Data, out var data);
+            var (result, data) = await CreateSplatDataAsync(fetchResult.Data);
             if (result != CreateSplatDataResultType.Success)
             {
                 InvokeLoadingStatusChanged(LoadingStatus.ModelLoadError, onLoadingStatusChanged);
@@ -48,45 +44,43 @@ namespace HoloLab.Spirare.Components.SplatVfx
             return (true, splatObject);
         }
 
-        private static CreateSplatDataResultType TryCreateSplatData(byte[] spzBytes, out SplatData splatData)
+        private static async Task<(CreateSplatDataResultType Result, SplatData SplatData)> CreateSplatDataAsync(byte[] spzBytes)
         {
-            using var inputFileStream = new MemoryStream(spzBytes);
-            using var gzipStream = new GZipStream(inputFileStream, CompressionMode.Decompress);
-            using var binaryReader = new BinaryReader(gzipStream);
-
-            if (SpzUtils.TryReadHeader(binaryReader, out var header) == false)
+            var (result, spzBody) = await Task.Run(() =>
             {
-                splatData = null;
-                return CreateSplatDataResultType.InvalidHeader;
+                using var inputFileStream = new MemoryStream(spzBytes);
+                using var gzipStream = new GZipStream(inputFileStream, CompressionMode.Decompress);
+                using var binaryReader = new BinaryReader(gzipStream);
+
+                if (SpzUtils.TryReadHeader(binaryReader, out var header) == false)
+                {
+                    return (CreateSplatDataResultType.InvalidHeader, null);
+                }
+
+                if (IsValidHeader(header) == false)
+                {
+                    return (CreateSplatDataResultType.InvalidHeader, null);
+                }
+
+                if (SpzUtils.TryReadBody(binaryReader, header, out var spzBody) == false)
+                {
+                    return (CreateSplatDataResultType.InvalidBody, null);
+                }
+
+                return (CreateSplatDataResultType.Success, spzBody);
+            });
+
+            if (result != CreateSplatDataResultType.Success)
+            {
+                return (result, null);
             }
 
-            if (IsValidHeader(header) == false)
-            {
-                splatData = null;
-                return CreateSplatDataResultType.InvalidHeader;
-            }
-
-            if (SpzUtils.TryReadBody(binaryReader, header, out var spzBody) == false)
-            {
-                splatData = null;
-                return CreateSplatDataResultType.InvalidBody;
-            }
-
-            splatData = ScriptableObject.CreateInstance<SplatData>();
-
-            /*
-             var arrays = LoadDataArrays(spzBytes);
-            data.PositionArray = arrays.position;
-            data.AxisArray = arrays.axis;
-            data.ColorArray = arrays.color;
-            data.ReleaseGpuResources();
-            */
-
+            var splatData = ScriptableObject.CreateInstance<SplatData>();
             splatData.PositionArray = spzBody.Positions;
             splatData.AxisArray = spzBody.Axis;
             splatData.ColorArray = spzBody.Colors;
 
-            return CreateSplatDataResultType.Success;
+            return (CreateSplatDataResultType.Success, splatData);
         }
 
         private static bool IsValidHeader(SpzHeader header)
@@ -110,57 +104,6 @@ namespace HoloLab.Spirare.Components.SplatVfx
             }
 
             return true;
-        }
-
-#pragma warning disable CS0649
-
-        private struct ReadData
-        {
-            public float px, py, pz;
-            public float sx, sy, sz;
-            public byte r, g, b, a;
-            public byte rw, rx, ry, rz;
-        }
-
-#pragma warning restore CS0649
-
-        private static (Vector3[] position, Vector3[] axis, Color[] color) LoadDataArrays(byte[] splatBytes)
-        {
-            var bytes = new Span<byte>(splatBytes);
-            var count = bytes.Length / 32;
-
-            var source = MemoryMarshal.Cast<byte, ReadData>(bytes);
-
-            var position = new Vector3[count];
-            var axis = new Vector3[count * 3];
-            var color = new Color[count];
-
-            for (var i = 0; i < count; i++)
-                ParseReadData(source[i],
-                              out position[i],
-                              out axis[i * 3],
-                              out axis[i * 3 + 1],
-                              out axis[i * 3 + 2],
-                              out color[i]);
-
-            return (position, axis, color);
-        }
-
-        [BurstCompile]
-        private static void ParseReadData(in ReadData src,
-                           out Vector3 position,
-                           out Vector3 axis1,
-                           out Vector3 axis2,
-                           out Vector3 axis3,
-                           out Color color)
-        {
-            var rv = (math.float4(src.rx, src.ry, src.rz, src.rw) - 128) / 128;
-            var q = math.quaternion(rv.x, -rv.y, rv.z, -rv.w);
-            position = math.float3(src.px, -src.py, src.pz);
-            axis1 = math.mul(q, math.float3(src.sx, 0, 0));
-            axis2 = math.mul(q, math.float3(0, src.sy, 0));
-            axis3 = math.mul(q, math.float3(0, 0, src.sz));
-            color = (Vector4)math.float4(src.r, src.g, src.b, src.a) / 255;
         }
 
         private static class SpzUtils
