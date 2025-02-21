6d9fdba91d984dd82370cadfed470969d800fcc0
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2025-02-21 17:03:57 +0900

    update

--- packages/jp.co.hololab.spirare.cesium3dmaps/HttpServer/Scripts/Cesium3dMapsHttpController.cs
+++ packages/jp.co.hololab.spirare.cesium3dmaps/HttpServer/Scripts/Cesium3dMapsHttpController.cs
@@ -62,6 +62,7 @@ namespace HoloLab.Spirare.Cesium3DMaps.HttpServer
 <html>
   <head>
     <meta charset=""UTF-8"">
+    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
   </head>
 
   <body>
