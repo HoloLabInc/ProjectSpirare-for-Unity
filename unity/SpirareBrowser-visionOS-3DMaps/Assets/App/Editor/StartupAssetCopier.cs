c56e3e7304b1f027b0971d21c859125bdc2a0046
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2025-02-25 13:26:56 +0900

    update

--- unity/SpirareBrowser-visionOS-3DMaps/Assets/App/Editor/StartupAssetCopier.cs
+++ unity/SpirareBrowser-visionOS-3DMaps/Assets/App/Editor/StartupAssetCopier.cs
@@ -8,11 +8,30 @@ namespace HoloLab.Spirare.Browser.VisionOS3DMaps.Editor
     [InitializeOnLoad]
     internal class StartupAssetCopier
     {
+        private class CopyAssetPath
+        {
+            public string SourcePath;
+            public string SourceGuid;
+            public string DestinationPath;
+        }
+
         private static readonly string sessionKey = "_SpirareBrowserVisionOS3DMaps_StartupAssetCopier";
 
-        private static readonly string sourcePath = "Assets/App/Resources~/GooglePhotorealistic3DTilesSourceSettings.asset";
-        private static readonly string sourceGuid = "7f948eef0a404a64eb497bdaced032b5";
-        private static readonly string destinationPath = "Assets/Resources/Spirare/GooglePhotorealistic3DTilesSourceSettings.asset";
+        private static readonly CopyAssetPath[] copyAssetPaths = new[]
+        {
+            new CopyAssetPath
+            {
+                SourcePath = "Assets/App/Resources~/GooglePhotorealistic3DTilesSourceSettings.asset",
+                SourceGuid = "7f948eef0a404a64eb497bdaced032b5",
+                DestinationPath = "Assets/Resources/Spirare/GooglePhotorealistic3DTilesSourceSettings.asset",
+            },
+            new CopyAssetPath
+            {
+                SourcePath = "Assets/App/Resources~/BaseMapSettings.asset",
+                SourceGuid = "7e60df89d8dadeb45a48f2e12f28b446",
+                DestinationPath = "Assets/Resources/Spirare/BaseMapSettings.asset",
+            }
+        };
 
         private static bool IsNewSession
         {
@@ -35,7 +54,10 @@ namespace HoloLab.Spirare.Browser.VisionOS3DMaps.Editor
                 return;
             }
 
-            CopyAssetUtility.TryCopyAsset(sourcePath, destinationPath, sourceGuid, copyMetaFile: true);
+            foreach (var copyAssetPath in copyAssetPaths)
+            {
+                CopyAssetUtility.TryCopyAsset(copyAssetPath.SourcePath, copyAssetPath.DestinationPath, copyAssetPath.SourceGuid, copyMetaFile: true);
+            }
         }
     }
 }
