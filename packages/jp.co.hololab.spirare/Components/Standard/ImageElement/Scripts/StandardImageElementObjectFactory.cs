1d9a30dee5dbc2765ce37c5e1e46896592845351
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2025-05-22 11:14:53 +0900

    update

--- packages/jp.co.hololab.spirare/Components/Standard/ImageElement/Scripts/StandardImageElementObjectFactory.cs
+++ packages/jp.co.hololab.spirare/Components/Standard/ImageElement/Scripts/StandardImageElementObjectFactory.cs
@@ -10,11 +10,20 @@ namespace HoloLab.Spirare
 
         public override GameObject CreateObject(PomlImageElement element, PomlLoadOptions loadOptions, Transform parentTransform = null)
         {
+            var pipeline = RenderPipelineUtility.GetRenderPipelineType();
+            var unlitMaterialType = pipeline switch
+            {
+                RenderPipelineType.BuiltIn => UnlitMaterialType.Unlit_Color,
+                RenderPipelineType.URP => UnlitMaterialType.UniversalRenderPipeline_Unlit,
+                _ => UnlitMaterialType.Unlit_Color,
+            };
+
             var imageElementComponent = Instantiate(imageElementComponentPrefab, parentTransform);
             imageElementComponent.name = "image";
 
             imageElementComponent.GetComponent<PomlObjectElementComponent>().Initialize(element);
             imageElementComponent.Initialize(element, loadOptions);
+            imageElementComponent.UnlitMaterialType = unlitMaterialType;
             _ = imageElementComponent.UpdateGameObject();
 
             return imageElementComponent.gameObject;
